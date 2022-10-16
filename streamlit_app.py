c3b29c78f0e26ea99f981af6dbf458ad46b94695
Mehdi Keshavarz Motlagh <mehdi.keshavarz@live.com>
2022-10-16 20:31:15 +0200

    Update streamlit_app.py
    Added another menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spanish & Rocket Smoothi')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado Toast')
+
+
+
+streamlit.header('Build Your Own Fruit Smoothie')
