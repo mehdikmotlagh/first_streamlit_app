43d7727cacaff46e9a79b3d495b4e6a175b9a8f6
Mehdi Keshavarz Motlagh <mehdi.keshavarz@live.com>
2022-10-28 16:11:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('Avacado Toast')
 
 import requests
 
-streamlit.header('Build Your Own Fruit Smoothie')
+//streamlit.header('Build Your Own Fruit Smoothie')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -23,6 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
