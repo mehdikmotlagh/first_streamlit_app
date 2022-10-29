8e7baec8e75f7f32e3a10b1c9bd29045808470f7
Mehdi Keshavarz Motlagh <mehdi.keshavarz@live.com>
2022-10-29 20:57:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-import snowflake-connector-python
+import snowflake.connector
 streamlit.title('My Parents New Healthy Dinner!')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
