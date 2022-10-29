7337d0c1144069aa74fb46f47dcca5701f087a8f
Mehdi Keshavarz Motlagh <mehdi.keshavarz@live.com>
2022-10-29 21:10:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,14 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load contains:")
 streamlit.text(my_data_row)
+#streamlit.text("Hello from Snowflake:")
+
+
 streamlit.title('My Parents New Healthy Dinner!')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
