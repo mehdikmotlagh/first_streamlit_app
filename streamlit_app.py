7e10688f58807f9e78d51a73c9c2919353c04912
Mehdi Keshavarz Motlagh <mehdi.keshavarz@live.com>
2022-10-29 21:12:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load contains:")
+streamlit.datafarme(my_data_row)
 #streamlit.text("Hello from Snowflake:")
 
 
